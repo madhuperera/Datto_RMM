4074e607e6bdc77a0b4c8129811fcc38e9754ce5
Madhu Perera <37104267+madhuperera@users.noreply.github.com>
2022-03-11 20:06:02 +1300

    Small Function to use within other scripts so your CRM software will have more information for person who is managing tickets.

---
+++ Functions/Get-DeviceDetails.ps1
@@ -0,0 +1,10 @@
+function Get-DeviceDetails
+{
+	
+	$Date_String = Get-Date -Format "dddd dd/MM/yyyy HH:mm:ss"
+	$ComputerName = $ENV:COMPUTERNAME
+	
+	$DeviceDetails = "Script running on the Computer: $ComputerName on $Date_String"
+	return $DeviceDetails
+
+}
---
+++ Functions/Update-OutputOnExit.ps1
@@ -0,0 +1,20 @@
+function Update-OutputOnExit
+{
+    param
+    (
+        [String] $UDF_Value,
+        [bool] $ExitCode,
+        [String] $Results,
+        [String] $Registry_Value
+    )
+
+    if ($UDF_Value)
+    {
+        New-ItemProperty -Path HKLM:\SOFTWARE\CentraStage\ -Name $UDF_Value -PropertyType String -Value $Registry_Value -Force
+    }
+        
+    write-host '<-Start Result->' -ErrorAction SilentlyContinue
+    write-host "STATUS=$Results" -ErrorAction SilentlyContinue
+    write-host '<-End Result->' -ErrorAction SilentlyContinue
+    exit $ExitCode
+}
