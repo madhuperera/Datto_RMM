53dfdcdb1c6c6cd8fedb068fab0bd76ef3d543ef
Madhu @ EIT <madhu@essentialit.co.nz>
2022-06-22 17:22:31 +1200

    Updated the Test Registry Key Function

--- Functions/Test-RegistryKeyValue.ps1
+++ Functions/Test-RegistryKeyValue.ps1
@@ -5,30 +5,22 @@ function Test-RegistryKeyValue
         [String] $F_Reg_Key_Parent_Path,
         [String] $F_Reg_Key_Name,
         [String] $F_Reg_Key_Value_Name,
-        [String] $F_Reg_Key_Value_Data,
-        [ValidateSet("String","ExpandString","Binary","DWord","MultiString","Qword")] $F_Reg_Key_Value_Type
+        [String] $F_Reg_Key_Value_Data
+        # [ValidateSet("String","ExpandString","Binary","DWord","MultiString","Qword")] $F_Reg_Key_Value_Type
     )
-    [bool] $ExitWithError = $true
-    [bool] $ExitWithNoError = $false
 
-    if(!(Test-Path -Path "$F_Reg_Key_Parent_Path\$Reg_Key_Name" -PathType Container))
+    [bool] $ExitValue = $false
+
+    if(Test-Path -Path "$F_Reg_Key_Parent_Path\$Reg_Key_Name" -PathType Container)
     {
-        New-Item -Path $Reg_Key_Parent_Path -Name $Reg_Key_Name -ItemType Conatiner -Force
-
-        if ($Reg_Key_Value_Data -and $Reg_Key_Value_Name)
+        if (Get-ItemProperty -Path "$F_Reg_Key_Parent_Path\$F_Reg_Key_Name" -Name $F_Reg_Key_Value_Name)
         {
-            try
+            if ((Get-ItemPropertyValue -Path "$F_Reg_Key_Parent_Path\$F_Reg_Key_Name" -Name $F_Reg_Key_Value_Name) -eq $F_Reg_Key_Value_Data)
             {
-                New-ItemProperty -Path "$Reg_Key_Parent_Path\$Reg_Key_Name" -Name $Reg_Key_Value_Name -PropertyType String -Value $Reg_Key_Value_Data -Force
+                $ExitValue = $true
             }
-            catch
-            {
-                exit $ExitWithError
-            }
-            
         }
     }
 
-    
-    exit $ExitWithNoError
+    return $ExitValue
 }
