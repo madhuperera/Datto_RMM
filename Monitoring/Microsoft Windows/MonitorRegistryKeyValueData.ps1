e7d68674fca85039261de4fe2cdb0b0d003ed262
Madhu @ EIT <madhu@essentialit.co.nz>
2022-06-28 17:25:50 +1200

    Moved Variables around in Registry Key Monitoring Script.

--- Monitoring/Microsoft Windows/MonitorRegistryKeyValueData.ps1
+++ Monitoring/Microsoft Windows/MonitorRegistryKeyValueData.ps1
@@ -2,10 +2,10 @@
 [String] $SReg_Key_Name = $ENV:DRMM_SReg_Key_Name # "DeviceTagging"
 [String] $SReg_Key_Value_Name = $ENV:DRMM_SReg_Key_Value_Name # "Group"
 [String] $SReg_Key_Value_Data = $ENV:DRMM_SReg_Key_Value_Data # "New_Zealand"
+[String] $SUDF_Value = $ENV:DRMM_SUDF_Value # Ex: Custom20
+[String] $SUDF_Registry_Data = $ENV:DRMM_SUDF_Registry_Data # Ex: "New Zealand"
 [bool] $ExitWithError = $true
 [bool] $ExitWithNoError = $false
-[String] $UDF_Value = "CHANGE ME" # Ex: Custom20
-[String] $Registry_Value = $SReg_Key_Value_Data # Ex: "New_Zealand"
 
 function Get-DeviceDetails
 {
@@ -67,9 +67,9 @@ function Test-RegistryKeyValue
 
 if (Test-RegistryKeyValue -F_Reg_Key_Parent_Path $SReg_Key_Parent_Path -F_Reg_Key_Name $SReg_Key_Name -F_Reg_Key_Value_Name $SReg_Key_Value_Name -F_Reg_Key_Value_Data $SReg_Key_Value_Data)
 {
-    Update-OutputOnExit -ExitCode $ExitWithNoError -Results "We found the matching Registry Key, Value and Data." -UDF_Value $UDF_Value -Registry_Value $Registry_Value
+    Update-OutputOnExit -ExitCode $ExitWithNoError -Results "We found the matching Registry Key, Value and Data." -UDF_Value $SUDF_Value -Registry_Value $SUDF_Registry_Data
 }
 else
 {
-    Update-OutputOnExit -ExitCode $ExitWithError -Results "Oh No! We did not find what you were after." -UDF_Value $UDF_Value -Registry_Value $Registry_Value
+    Update-OutputOnExit -ExitCode $ExitWithError -Results "Oh No! We did not find what you were after."
 }
