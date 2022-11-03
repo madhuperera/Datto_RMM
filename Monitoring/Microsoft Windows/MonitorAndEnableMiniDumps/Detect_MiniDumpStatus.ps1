2c2ed19f724b45e69b5efa872922e583bdc6d318
Madhu @ EIT <madhu@essentialit.co.nz>
2022-11-03 14:04:18 +1300

    Changes to Mini Dump Script

---
+++ Monitoring/Microsoft Windows/MonitorAndEnableMiniDumps/Detect_MiniDumpStatus.ps1
@@ -0,0 +1,72 @@
+[String] $DesiredDebugInfoType = 3
+
+function Get-DeviceDetails
+{
+	
+	$Date_String = Get-Date -Format "dddd dd/MM/yyyy HH:mm:ss"
+	$ComputerName = $ENV:COMPUTERNAME
+	
+	$DeviceDetails = "Script running on the Computer: $ComputerName on $Date_String`n`n"
+	return $DeviceDetails
+
+}
+
+Get-DeviceDetails
+
+function Update-OutputOnExit
+{
+    param
+    (
+        [String] $UDF_Value,
+        [bool] $ExitCode,
+        [String] $Results,
+        [String] $Registry_Value
+    )
+
+    if ($UDF_Value -and $Registry_Value)
+    {
+        New-ItemProperty -Path HKLM:\SOFTWARE\CentraStage\ -Name $UDF_Value -PropertyType String -Value $Registry_Value -Force
+    }
+        
+    write-host '<-Start Result->' -ErrorAction SilentlyContinue
+    write-host "STATUS=$Results" -ErrorAction SilentlyContinue
+    write-host '<-End Result->' -ErrorAction SilentlyContinue
+    exit $ExitCode
+}
+
+[bool] $ExitWithError = $true
+[bool] $ExitWithNoError = $false
+
+function Get-DebugInfoType
+{
+    [String] $DebugInfoType = ""
+    try
+    {
+        $DebugInfoType = ((wmic RECOVEROS get DebugInfoType | Select-Object -Skip 2) -split " ")[0]
+        return $DebugInfoType
+    }
+    catch
+    {
+        return "Error"
+    }
+}
+
+# Getting the current configuration
+[String] $CurrentDebugInfoType = Get-DebugInfoType
+
+switch ($CurrentDebugInfoType)
+{
+    "Error" 
+        {
+            Update-OutputOnExit -ExitCode $ExitWithError -Results "Something went wrong trying to check the Debug Info Type!"
+        }
+    $DesiredDebugInfoType
+        {
+            Update-OutputOnExit -ExitCode $ExitWithNoError -Results "No Reboot Needed. The System was already configured with the correct Debug Info Type"
+        }
+    Default
+        {
+            Update-OutputOnExit -ExitCode $ExitWithError -Results "$CurrentDebugInfoType does not what is desired: $DesiredDebugInfoType"
+        }
+}
+
