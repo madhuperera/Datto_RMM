0110afadf0599fab5d841b73b3b25a88c068e553
Madhu Perera <madhu@sonitlo.com>
2023-02-27 18:41:31 +1300

    Sync

---
+++ Scripts/Windows/Collect_Information.ps1
@@ -0,0 +1,40 @@
+# Author: Madhu Perera
+# Reference: 
+
+
+
+# --------------- FUNCTIONS ------------------
+function Get-DeviceDetails
+{
+	
+	$Date_String = Get-Date -Format "dddd dd/MM/yyyy HH:mm:ss"
+	$ComputerName = $ENV:COMPUTERNAME
+	
+	$DeviceDetails = "Script running on the Computer: $ComputerName on $Date_String`n`n"
+	return $DeviceDetails
+
+}
+
+function Update-OutputOnExit
+{
+    param
+    (
+        [String] $UDF_Value,
+        [bool] $ExitCode,
+        [String] $Results,
+        [String] $Registry_Value
+    )
+
+    if ($UDF_Value -and $Registry_Value)
+    {
+        New-ItemProperty -Path HKLM:\SOFTWARE\CentraStage\ -Name $UDF_Value -PropertyType String -Value $Registry_Value -Force
+    }
+        
+    write-host '<-Start Result->' -ErrorAction SilentlyContinue
+    write-host "STATUS=$Results" -ErrorAction SilentlyContinue
+    write-host '<-End Result->' -ErrorAction SilentlyContinue
+    exit $ExitCode
+}
+
+# ----------------- MAIN -----------------------------
+
